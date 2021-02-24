3459bde68fac1e3a31c576958b348070ebbf379c
stack07142 <stack.07142@gmail.com>
2021-02-24 09:36:14 +0900

    imageCapture use case

--- app/src/main/java/k/t/cameraxsample/MainActivity.kt
+++ app/src/main/java/k/t/cameraxsample/MainActivity.kt
@@ -2,11 +2,13 @@ package k.t.cameraxsample
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
@@ -15,6 +17,8 @@ import androidx.databinding.DataBindingUtil
 import k.t.cameraxsample.databinding.ActivityMainBinding
 import timber.log.Timber
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -41,12 +45,16 @@ class MainActivity : AppCompatActivity() {
 
         outputDirectory = getOutputDirectory()
         cameraExecutor = Executors.newSingleThreadExecutor()
+
+        binding.btnCapture.setOnClickListener {
+            takePhoto()
+        }
     }
 
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-        cameraProviderFuture.addListener(Runnable {
+        cameraProviderFuture.addListener({
             // Used to bind the lifecycle of cameras to the lifecycle owner
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
@@ -57,6 +65,9 @@ class MainActivity : AppCompatActivity() {
                         it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
                     }
 
+            // ImageCapture
+            imageCapture = ImageCapture.Builder().build()
+
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
@@ -65,7 +76,7 @@ class MainActivity : AppCompatActivity() {
                 cameraProvider.unbindAll()
 
                 // Bind use cases to camera
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
             } catch (e: Exception) {
                 Timber.e(e, "Use case binding failed")
             }
@@ -75,7 +86,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun takePhoto() {
-        TODO("Not yet implemented")
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time-stamped output file to hold the image
+        val fileName = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis()) + ".jpg"
+        val photoFile = File(outputDirectory, fileName)
+
+        // Create output options object which contains file + metadata
+        val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Set up image capture listener, which is triggered after photo has been taken
+        imageCapture.takePicture(outputOption, ContextCompat.getMainExecutor(this),
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                        val savedUri = Uri.fromFile(photoFile)
+                        val msg = "Photo capture succeeded: $savedUri"
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        Timber.d(msg)
+                    }
+
+                    override fun onError(e: ImageCaptureException) {
+                        Timber.e(e, "Photo capture failed")
+                    }
+                })
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
