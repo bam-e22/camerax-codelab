7b0d6ca0315d1861970fcde876de05132bc77db0
stack07142 <stack.07142@gmail.com>
2021-03-02 11:19:33 +0900

    immersive mode

--- app/src/main/java/k/t/cameraxsample/MainActivity.kt
+++ app/src/main/java/k/t/cameraxsample/MainActivity.kt
@@ -1,6 +1,10 @@
 package k.t.cameraxsample
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import k.t.cameraxsample.databinding.ActivityMainBinding
@@ -14,5 +18,39 @@ class MainActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) setImmersiveMode()
+    }
+
+    private fun setImmersiveMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window?.run {
+                setDecorFitsSystemWindows(false)
+                insetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                insetsController?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = FLAGS_FULLSCREEN
+        }
+    }
+
+    companion object {
+        /** Combination of all flags required to put activity into immersive mode */
+        @Suppress("DEPRECATION")
+        private const val FLAGS_FULLSCREEN =
+                //View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        //View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+        /** Milliseconds used for UI animations */
+        const val ANIMATION_FAST_MILLIS = 50L
+        const val ANIMATION_SLOW_MILLIS = 100L
+        private const val IMMERSIVE_FLAG_TIMEOUT = 500L
     }
 }
